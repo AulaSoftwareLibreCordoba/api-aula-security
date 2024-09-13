2b4f9a2ca22ab83d3efb7bcbc6a61cdb9ba180e6
juan <jdeomoya@gmail.com>
2024-09-13 21:01:46 +0200

    casi completada la verificacion por correo

--- API-AULA/api-aula-security/src/main/java/com/universe/service/UserServiceImpl.java
+++ API-AULA/api-aula-security/src/main/java/com/universe/service/UserServiceImpl.java
@@ -8,6 +8,7 @@ import com.universe.util.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import com.universe.service.EnvioEmailService;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,6 +24,8 @@ public class UserServiceImpl {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private EnvioEmailService envioEmailService;
 
     @Autowired
     private RoleRepository roleRepository;
@@ -34,11 +37,22 @@ public class UserServiceImpl {
     }
 
     public void addUser(UserEntity user) {
-
         // Codificar la contraseña
         user.setPassword(passwordEncoder.encode(user.getPassword()));
 
+        // Guardar el usuario en la base de datos
         userRepository.save(user);
+
+        // Enviar correo al administrador con los detalles del nuevo usuario
+        String emailContent = "Se ha registrado un nuevo usuario con los siguientes detalles:\n" +
+                "Username: " + user.getUsername() + "\n" +
+                "Email: " + user.getEmail() + "\n";
+
+        envioEmailService.sendEmail(
+                "auladesoftwarelibre@gmail.com", // Destinatario
+                "Nuevo Usuario Registrado",      // Asunto del correo
+                emailContent                     // Cuerpo del mensaje con los detalles del usuario
+        );
     }
 
 }
